4656e85e2363aa6ae23b80cf2ad87577f8d010a3
Richard Chao <r.chao03@gmail.com>
2016-07-12 15:23:26 -0400

    sessions done

--- browser/app/main.js
+++ browser/app/main.js
@@ -2,6 +2,8 @@
 
 var app = angular.module('auther', ['ui.router']);
 
+
+
 app.config(function ($urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
--- server/app/index.js
+++ server/app/index.js
@@ -1,7 +1,10 @@
-'use strict'; 
+'use strict';
 
 var app = require('express')();
 var path = require('path');
+var session = require('express-session');
+var User = require('../api/users/user.model');
+
 
 app.use(require('./logging.middleware'));
 
@@ -11,6 +14,41 @@ app.use(require('./statics.middleware'));
 
 app.use('/api', require('../api/api.router'));
 
+
+app.use(session({
+  // this mandatory configuration ensures that session IDs are not predictable
+  secret: 'tongiscool' // or whatever you like
+}));
+
+app.use(function (req, res, next) {
+  console.log('session', req.session);
+  next();
+});
+
+app.post('/login', function (req, res, next) {
+  User.findOne({
+    where: req.body
+  })
+  .then(function (user) {
+    if (!user) {
+      res.sendStatus(401);
+    } else {
+      req.session.userId = user.id;
+      res.sendStatus(204);
+    }
+    console.log(req.session)
+  })
+  .catch(next);
+});
+
+app.use('/api', function (req, res, next) {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter);
+  next();
+});
+
+
+
 var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
 var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html');
 validFrontendRoutes.forEach(function (stateRoute) {
@@ -18,6 +56,7 @@ validFrontendRoutes.forEach(function (stateRoute) {
     res.sendFile(indexPath);
   });
 });
+
 
 app.use(require('./error.middleware'));
 
